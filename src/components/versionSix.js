fcfd2bd48b681cc228aff7b0b16505ec81ed8a17
santoshTECTUM <santosh.tectum.com>
2023-12-14 17:11:54 +0530

    version 6

--- src/components/versionSix.js
+++ src/components/versionSix.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import { Button } from 'reactstrap'
 
 const VersionSix = () => {
+    const ref = useRef(0)
+
+    const dataCounte = () => {
+        ref.current = ref.current + 1
+    }
+
     return (
         <div>
-            version 6.0.0
+            <Button onClick={() => { dataCounte }} >useRef increment</Button>
+            {ref.current}
         </div>
     )
 }
